b7be0b82a1f4d58383782c61c405ae583eb91f89
Rafael <37466798+likeavenus@users.noreply.github.com>
2020-03-06 22:06:32 +0300

    Updates

---
+++ precache-manifest.c17433e874077cd616acc031d2d78c52.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8e40243bc623c279394ae8ca3486c6",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "a12d5da4709a839ea294",
+    "url": "/weather-app/static/css/main.36fae3f1.chunk.css"
+  },
+  {
+    "revision": "7ae24aca0349666d2b83",
+    "url": "/weather-app/static/js/2.f26c0cb0.chunk.js"
+  },
+  {
+    "revision": "a12d5da4709a839ea294",
+    "url": "/weather-app/static/js/main.f1e1e669.chunk.js"
+  },
+  {
+    "revision": "8605d821609a006adaaa",
+    "url": "/weather-app/static/js/runtime-main.49c2d159.js"
+  },
+  {
+    "revision": "278321edc4a28fc3f2441b1472e9347a",
+    "url": "/weather-app/static/media/Ubuntu-Bold.278321ed.woff"
+  },
+  {
+    "revision": "c636b9b677fbd28b6596bf4ff1043f44",
+    "url": "/weather-app/static/media/Ubuntu-Bold.c636b9b6.woff2"
+  },
+  {
+    "revision": "37fefefcfd1a1e4dd4b05c2125ed78cf",
+    "url": "/weather-app/static/media/Ubuntu-Medium.37fefefc.woff"
+  },
+  {
+    "revision": "51496ddf1ce33c063212ff1323c0e7b7",
+    "url": "/weather-app/static/media/Ubuntu-Medium.51496ddf.woff2"
+  },
+  {
+    "revision": "b7350427f5d9fbceabb3325336d69531",
+    "url": "/weather-app/static/media/Ubuntu-Regular.b7350427.woff2"
+  },
+  {
+    "revision": "ba4e7809d474b7b6936d09346695c3ae",
+    "url": "/weather-app/static/media/Ubuntu-Regular.ba4e7809.woff"
+  },
+  {
+    "revision": "da1db2893fee46783f85c2f9602879ad",
+    "url": "/weather-app/static/media/preloader.da1db289.svg"
+  }
+]);
